61d32cc3a46e0bf4a5bb9a8551dfb850678a605c
zhangchao <salman.c.zhang@magugi.com>
2023-01-18 15:10:57 +0800

    自动获取最新结单

--- src/activities/manage/BillManageActivity.js
+++ src/activities/manage/BillManageActivity.js
@@ -66,12 +66,12 @@ class BillManageOtherView extends React.Component {
         });
     }
 
-    // componentDidMount() {
-    //     this.props.navigation.setParams({loadData: this.loadData});
-    //     const formatNowTime = moment().format('YYYY-MM-DD');
-    //     this.setState({selectTime: formatNowTime});
-    //     this.loadData();
-    // }
+    componentDidMount() {
+        this.props.navigation.setParams({loadData: this.loadData});
+        const formatNowTime = moment().format('YYYY-MM-DD');
+        this.setState({selectTime: formatNowTime});
+        this.loadData();
+    }
 
     componentWillUnmount() {
         this.props.reset && this.props.reset();
