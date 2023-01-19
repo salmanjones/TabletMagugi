796b5478f70a1605dea36d2c5337be71b7f55c00
zhangchao <salman.c.zhang@magugi.com>
2023-01-19 11:00:02 +0800

    环境切换

--- env.js
+++ env.js
@@ -1,9 +1,20 @@
 export default {
     ver: '3.1.3',
     api: {
+        // online
         bms: 'https://magi.magugi.com',
         app: 'https://jx-magi.magugi.com/datahandler',
+
+        // // test
         // bms: 'http://192.168.1.194:8000/bms',
         // app: 'http://192.168.1.194:8000/peafowl-datahandler-deploy/datahandler'
+
+        // // preview
+        // bms: 'http://192.168.1.56:8080/bms',
+        // app: 'http://192.168.1.56:8080/app/datahandler'
+
+        // // lex
+        // bms: 'http://192.168.1.21:8090/bms',
+        // app: 'http://192.168.1.21:8000/peafowl-datahandler-deploy/datahandler'
     }
 }
