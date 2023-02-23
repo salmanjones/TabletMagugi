656e883f5b213969842db9af8722400e106f8d47
zhangchao <salman.c.zhang@magugi.com>
2023-02-23 10:18:47 +0800

    升级版本

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export default {
-    ver: '3.1.5',
+    ver: '3.1.6',
     api: {
         // // online
         // bms: 'https://magi.magugi.com',
@@ -10,11 +10,11 @@ export default {
         // app: 'http://192.168.1.194:8000/peafowl-datahandler-deploy/datahandler'
 
         // // preview
-        bms: 'http://192.168.1.56:8080/bms',
-        app: 'http://192.168.1.56:8080/app/datahandler'
+        // bms: 'http://192.168.1.56:8080/bms',
+        // app: 'http://192.168.1.56:8080/app/datahandler'
 
-        // // lex
-        // bms: 'http://192.168.1.21:8090/bms',
-        // app: 'http://192.168.1.21:8000/peafowl-datahandler-deploy/datahandler'
+        // // 本机
+        bms: 'http://192.168.1.2:8080/bms',
+        app: 'http://192.168.1.2:8080/app/datahandler'
     }
 }
