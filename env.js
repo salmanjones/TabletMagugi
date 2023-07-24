79faf5786805ae3e1fb092ca77e94cdc2577bf31
zhangchao <salman.c.zhang@magugi.com>
2023-07-24 09:44:32 +0800

    api设置为新地址

--- env.js
+++ env.js
@@ -1,17 +1,17 @@
 export default {
     ver: '3.1.8',
     api: {
-        // // online
-        // bms: 'https://bms.magi2002.com',
-        // app: 'https://jx.magi2002.com/datahandler',
+        // online
+        bms: 'https://bms.magi2002.com',
+        app: 'https://jx.magi2002.com/datahandler',
 
         // test
         // bms: 'http://192.168.1.194:8000/bms',
         // app: 'http://192.168.1.194:8000/peafowl-datahandler-deploy/datahandler'
 
         // // preview
-        bms: 'http://192.168.1.56:8080/bms',
-        app: 'http://192.168.1.56:8080/app/datahandler',
+        // bms: 'http://192.168.1.56:8080/bms',
+        // app: 'http://192.168.1.56:8080/app/datahandler',
 
         // // 本机
         // bms: 'http://192.168.1.56:8080/bms',
