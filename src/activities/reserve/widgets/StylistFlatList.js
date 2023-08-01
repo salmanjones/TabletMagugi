c0841300bba6e0c8cf3378c7e42ce523d306672f
zhangchao <salman.c.zhang@magugi.com>
2023-08-01 19:28:44 +0800

    处理选中问题

--- src/activities/reserve/widgets/StylistFlatList.js
+++ src/activities/reserve/widgets/StylistFlatList.js
@@ -4,24 +4,37 @@ import {ReserveBoardStyles} from "../../../styles/ReserveBoard"
 
 export default React.memo(({reserveInfoArray, checkStylistEvent, reserveFlag}) => {
     const [checkIndex, setCheckIndex] = useState(0)
+    const [checkStaffId, setCheckStaffId] = useState('')
 
-    const checkItemEvent = React.useCallback((index)=>{
+    const checkItemEvent = React.useCallback((index, staffId)=>{
         // 命中颜色
         setCheckIndex(index)
+        setCheckStaffId(staffId)
         // 展示预约列表
         checkStylistEvent(index)
     }, [reserveFlag])
 
-    const Item = React.memo(({index, title, validCount, invalidCount}) => {
+    const Item = React.memo(({index, title, validCount, invalidCount, staffId}) => {
         const showName = title.length > 6 ? title.substring(0,6) : title
         const showText = reserveFlag == 'valid' ? `${showName}(${validCount})`:`${showName}(${invalidCount})`
-        return (
-            <TouchableOpacity style={ReserveBoardStyles.reserveStylistItemBox} onPress={()=>{checkItemEvent(index)}}>
-                <View style={index == checkIndex ? ReserveBoardStyles.reserveStylistItemActive:ReserveBoardStyles.reserveStylistItem}>
-                    <Text style={index == checkIndex ? ReserveBoardStyles.reserveStylistItemTextActive:ReserveBoardStyles.reserveStylistItemText}>{showText}</Text>
-                </View>
-            </TouchableOpacity>
-        )
+
+        if(checkStaffId && checkStaffId.length > 0){
+            return (
+                <TouchableOpacity style={ReserveBoardStyles.reserveStylistItemBox} onPress={()=>{checkItemEvent(index, staffId)}}>
+                    <View style={staffId == checkStaffId ? ReserveBoardStyles.reserveStylistItemActive:ReserveBoardStyles.reserveStylistItem}>
+                        <Text style={staffId == checkStaffId ? ReserveBoardStyles.reserveStylistItemTextActive:ReserveBoardStyles.reserveStylistItemText}>{showText}</Text>
+                    </View>
+                </TouchableOpacity>
+            )
+        }else{
+            return (
+                <TouchableOpacity style={ReserveBoardStyles.reserveStylistItemBox} onPress={()=>{checkItemEvent(index, staffId)}}>
+                    <View style={index == checkIndex ? ReserveBoardStyles.reserveStylistItemActive:ReserveBoardStyles.reserveStylistItem}>
+                        <Text style={index == checkIndex ? ReserveBoardStyles.reserveStylistItemTextActive:ReserveBoardStyles.reserveStylistItemText}>{showText}</Text>
+                    </View>
+                </TouchableOpacity>
+            )
+        }
     })
 
     return (
@@ -29,7 +42,7 @@ export default React.memo(({reserveInfoArray, checkStylistEvent, reserveFlag}) =
             data={reserveInfoArray}
             initialNumToRender={4}
             renderItem={
-                ({item, index}) => <Item index={index} title={item.staffName} validCount={item.staffNowReseverCount} invalidCount={item.staffPassReseverCount}/>
+                ({item, index}) => <Item staffId={item.staffId} index={index} title={item.staffName} validCount={item.staffNowReseverCount} invalidCount={item.staffPassReseverCount}/>
             }
             keyExtractor={item => item.staffId}/>
     )
