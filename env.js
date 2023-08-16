70d7473ce5fb171ea680b02f3280ae1193728a7f
zhangchao <salman.c.zhang@magugi.com>
2023-08-16 18:38:28 +0800

    更新为线上版本

--- env.js
+++ env.js
@@ -2,8 +2,8 @@ export default {
     ver: '3.2.2',
     api: {
         // online
-        // bms: 'https://bms.magi2002.com',
-        // app: 'https://jx.magi2002.com/datahandler',
+        bms: 'https://bms.magi2002.com',
+        app: 'https://jx.magi2002.com/datahandler',
 
         // test
         // bms: 'http://192.168.1.194:8000/bms',
@@ -14,7 +14,7 @@ export default {
         // app: 'http://192.168.1.56:8080/app/datahandler'
 
         // self
-        bms: 'http://127.0.0.1:8080/bms',
-        app: 'http://127.0.0.1:8080/app/datahandler',
+        // bms: 'http://192.168.1.2:8080/bms',
+        // app: 'http://192.168.1.2:8080/app/datahandler',
     }
 }
