5e963af2c145f7d43da73d1f105b98570696988f
zhangchao <salman.c.zhang@magugi.com>
2023-09-07 18:33:13 +0800

    替换为真实环境

--- env.js
+++ env.js
@@ -2,16 +2,16 @@ export default {
     ver: '3.2.4',
     api: {
         // online
-        // bms: 'https://bms.magi2002.com',
-        // app: 'https://jx.magi2002.com/datahandler',
+        bms: 'https://bms.magi2002.com',
+        app: 'https://jx.magi2002.com/datahandler',
 
         // test
         // bms: 'http://192.168.1.194:8000/bms',
         // app: 'http://192.168.1.194:8000/peafowl-datahandler-deploy/datahandler'
 
         // 测试
-        bms: 'http://192.168.1.56:8080/bms',
-        app: 'http://192.168.1.56:8080/app/datahandler'
+        // bms: 'http://192.168.1.56:8080/bms',
+        // app: 'http://192.168.1.56:8080/app/datahandler'
 
         // self
         // bms: 'http://192.168.1.2:8080/bms',
