8bd03b7bc0a2d1fcedd69a636b32c09666d653d1
zhangchao <salman.c.zhang@magugi.com>
2024-01-09 09:55:10 +0800

    升级系统版本

--- env.js
+++ env.js
@@ -1,15 +1,15 @@
 export default {
-    ver: '3.3.0',
+    ver: '3.3.1',
     api: {
         // online
-        // bms: 'https://bms.magi2002.com',
-        // app: 'https://jx.magi2002.com/datahandler',
-        // lwm: 'https://lwapi.magi2002.com',
+        bms: 'https://bms.magi2002.com',
+        app: 'https://jx.magi2002.com/datahandler',
+        lwm: 'https://lwapi.magi2002.com',
 
         // test
-        bms: 'http://192.168.1.194:8000/bms',
-        app: 'http://192.168.1.194:8000/peafowl-datahandler-deploy/datahandler',
-        lwm: 'http://linkwe.magugi.vip',
+        // bms: 'http://192.168.1.194:8000/bms',
+        // app: 'http://192.168.1.194:8000/peafowl-datahandler-deploy/datahandler',
+        // lwm: 'http://linkwe.magugi.vip',
 
         // 测试
         // bms: 'http://192.168.1.56:8080/bms',
--- src/components/manage/index.js
+++ src/components/manage/index.js
@@ -2,7 +2,6 @@ export * from './HeadeConsumeTitleLeft';
 export * from './AddConsumablesItem';
 export * from './ConsumableList';
 export * from './ConsumableSelectBox';
-export * from './AmendServicerModal'; //修改单据详情服务人
 export * from './SimulateKeyboardInpBox'; //小数点数字键盘
 export * from './StaffEditBar';
 export * from './BillingOrderSummary';
